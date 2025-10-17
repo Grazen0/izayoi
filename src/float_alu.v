dbbca852562e161afc94c041765dd175bb5c83e6
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-17 11:25:16 -0500

    feat: integrate multiplier into top-level alu

--- src/float_alu.v
+++ src/float_alu.v
@@ -1,3 +1,31 @@
+`default_nettype none
+
+`define OP_ADD 3'b000
+`define OP_SUB 3'b001
+`define OP_MUL 3'b010
+`define OP_DIV 3'b100
+
+module fp_decoder (
+    input wire [2:0] op_code,
+    input wire start,
+    input wire ready_in,
+    output wire adder_start,
+    output wire adder_ready_in,
+    output wire multiplier_start,
+    output wire multiplier_ready_in,
+    output wire divider_start,
+    output wire divider_ready_in
+);
+  assign adder_start = start && (op_code == `OP_ADD || op_code == `OP_SUB);
+  assign adder_ready_in = ready_in || (op_code != `OP_ADD && op_code != `OP_SUB);
+
+  assign multiplier_start = start && op_code == `OP_MUL;
+  assign multiplier_ready_in = ready_in || op_code != `OP_MUL;
+
+  assign divider_start = start && op_code == `OP_DIV;
+  assign divider_ready_in = ready_in || op_code != `OP_DIV;
+endmodule
+
 module float_alu #(
     parameter P = 23,
     parameter E = 8,
@@ -18,10 +46,20 @@ module float_alu #(
     output reg [N-1:0] result,
     output reg [4:0] flags
 );
-  localparam OP_ADD = 3'b000;
-  localparam OP_SUB = 3'b001;
-  localparam OP_MUL = 3'b010;
-  localparam OP_DIV = 3'b100;
+  wire adder_start, adder_ready_in, multiplier_start, multiplier_ready_in,
+    divider_start, divider_ready_in;
+
+  fp_decoder decoder (
+      .op_code(op_code),
+      .start(start),
+      .ready_in(ready_in),
+      .adder_start(adder_start),
+      .adder_ready_in(adder_ready_in),
+      .multiplier_start(multiplier_start),
+      .multiplier_ready_in(multiplier_ready_in),
+      .divider_start(divider_start),
+      .divider_ready_in(divider_ready_in)
+  );
 
   wire adder_valid, adder_ready;
   wire [N-1:0] adder_result;
@@ -35,8 +73,8 @@ module float_alu #(
       .mode_fp(mode_fp),
       .round_mode(round_mode),
       .sub(op_code[0]),
-      .start(start && (op_code == OP_ADD || op_code == OP_SUB)),
-      .ready_in(ready_in || (op_code != OP_ADD && op_code != OP_SUB)),
+      .start(adder_start),
+      .ready_in(adder_ready_in),
 
       .valid_out(adder_valid),
       .ready_out(adder_ready),
@@ -44,13 +82,39 @@ module float_alu #(
       .flags(adder_flags)
   );
 
+  wire multiplier_valid, multiplier_ready;
+  wire [N-1:0] multiplier_result;
+  wire [  4:0] multiplier_flags;
+
+  fp_multiplier multiplier (
+      .clk(clk),
+      .rst_n(rst_n),
+      .op_a(op_a),
+      .op_b(op_b),
+      .mode_fp(mode_fp),
+      .round_mode(round_mode),
+      .start(multiplier_start),
+      .ready_in(multiplier_ready_in),
+
+      .valid_out(multiplier_valid),
+      .ready_out(multiplier_ready),
+      .result(multiplier_result),
+      .flags(multiplier_flags)
+  );
+
   always @(*) begin
     case (op_code)
-      OP_ADD: begin
+      `OP_ADD, `OP_SUB: begin
         valid_out = adder_valid;
         ready_out = adder_ready;
         result = adder_result;
         flags = adder_flags;
+      end
+      `OP_MUL: begin
+        valid_out = multiplier_valid;
+        ready_out = multiplier_ready;
+        result = multiplier_result;
+        flags = multiplier_flags;
       end
       default: begin
         valid_out = 1'b0;
--- tb/float_alu_tb.v
+++ tb/float_alu_tb.v
@@ -23,14 +23,58 @@ module float_alu_tb ();
     end
   end
 
+  float_alu izayoi (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .op_a(op_a),
+      .op_b(op_b),
+      .op_code(op_code),
+      .round_mode(round_mode),
+      .mode_fp(mode_fp),
+      .start(start),
+      .ready_in(1'b1),
+
+      .valid_out(valid_out),
+      .ready_out(ready_out),
+      .result(result),
+      .flags(flags)
+  );
+
   initial begin
     $dumpvars(0, float_alu_tb);
 
     clk   = 1;
     rst_n = 0;
 
-    #5 rst_n = 1;
+    #3 rst_n = 1;
 
-    $finish();
+    send_op(32'h41A6_0000, 32'h4010_0000, 3'b010);  // 20.75 + 2.25 = 41B8_0000 (23.0)
+    send_op(32'h4160_0000, 32'hC144_0000, 3'b010);  // 14 - 12.25 = 3FE0_0000 (1.75)
+    send_op(32'h4102_0000, 32'hC104_0000, 3'b010);  // 8.125 - (-8.25) = 4183_0000 (16.375)
+    send_op(32'h40A8_0000, 32'h4194_0000, 3'b010);  // 5.25 + 18.5 = 41BE_0000 (23.75)
+    send_op(32'h4229_3333, 32'h4188_28F6, 3'b010);  // 42.3 - 17.02 = 41CA_3D70 (~25.27999)
+
+    #100 $finish();
   end
+
+  task send_op(input reg [31:0] a, input reg [31:0] b, input reg [2:0] op);
+    begin
+      op_a = a;
+      op_b = b;
+      op_code = op;
+      round_mode = 1'b0;  // nearest even
+      mode_fp = 1'b1;  // single
+
+      start = 1'b1;
+      @(posedge clk);
+      #2 start = 1'b0;
+      #1;
+
+      while (!valid_out) begin
+        @(posedge clk);
+        #1;
+      end
+    end
+  endtask
 endmodule
