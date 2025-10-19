34a7ec28f69cdd8f79aea32dc38a267571f35337
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-19 17:55:19 -0500

    feat: add div single testbenches

--- tb/float_alu/div_single_tb.v
+++ tb/float_alu/div_single_tb.v
@@ -58,33 +58,30 @@ module div_single_tb ();
     mode_fp = 1'b1;  // single
 
     // Normal cases
-    test(`OP_DIV, 32'h41A6_0000, 32'h4010_0000, 32'h423A_C000);  // 20.75 * 2.25 = 46.6875
-    test(`OP_DIV, 32'h4010_0000, 32'h41A6_0000, 32'h423A_C000);  // 2.25 * 20.75 = 46.6875
-    test(`OP_DIV, 32'h4160_0000, 32'hC144_0000, 32'hC32B_8000);  // 14 * (-12.25) = -171.5
-    test(`OP_DIV, 32'hC144_0000, 32'h4160_0000, 32'hC32B_8000);  // (-12.25) * 14 = -171.5
-    test(`OP_DIV, 32'h4102_0000, 32'h4104_0000, 32'h4286_1000);  // 8.125 * 8.25 = 67.03125
-    test(`OP_DIV, 32'h40A8_0000, 32'h4194_0000, 32'h42C2_4000);  // 5.25 * 18.5 = 97.125
-    test(`OP_DIV, 32'h4229_3333, 32'hC188_28F6, 32'hC433_FC8B);  // 42.3 * (-17.02) = -719.946
-    test(`OP_DIV, 32'h3DCC_CCCD, 32'h3E4C_CCCD, 32'h3CA3_D70B);  // 0.1 * 0.2 = ~0.02 (inexact)
+    test(`OP_DIV, 32'h41A6_0000, 32'h4010_0000, 32'h4113_8E38);  // 20.75 / 2.25 = 9.222...
+    test(`OP_DIV, 32'h4160_0000, 32'hC144_0000, 32'hBF92_4925);  // 14 / (-12.25) = -1.142...
+    test(`OP_DIV, 32'h4102_0000, 32'h4104_0000, 32'h3F7C_1F06);  // 8.125 / 8.25 = 0.9848...
+    test(`OP_DIV, 32'h40A8_0000, 32'h4194_0000, 32'h3E91_4C1D);  // 5.25 / 18.5 = 28.37
+    test(`OP_DIV, 32'h4229_3333, 32'hC188_28F6, 32'hC01F_0F57);  // 42.3 / (-17.02) = -2.485
+    test(`OP_DIV, 32'h3DCC_CCCD, 32'h3E4C_CCCD, 32'h3F00_0000);  // 0.1 / 0.2 = 0.5
 
     round_mode = 1'b1;  // round to zero
 
-    test(`OP_DIV, 32'h3DCC_CCCD, 32'h3E4C_CCCD, 32'h3CA3_D70B);  // 0.1 * 0.2 = ~0.3 (inexact)
-    test(`OP_DIV, 32'h7F7F_FFFF, 32'h7F7F_FFFF, `INF);  // 3.4e38 * 3.4e38 = Inf (overflow)
-    test(`OP_DIV, 32'h425A_CCCD, 32'h0E69_999A, 32'h1147_A7AF);  // 54.7 * 2.87e-30 = 1.5e-28
-    test(`OP_DIV, 32'h0000_0040, 32'h0000_0003,
-         `ZERO);  // 9e-44 * 4e-45 = 3.6e-88 (underflow, inexact)
-    test(`OP_DIV, 32'h4049_0FDB, 32'h402D_F854, 32'h4108_A2C0);  // pi * e = ~8.539 (inexact)
+    test(`OP_DIV, 32'h4228_0000, 32'h4228_0000, 32'h3F80_0000);  // 42.0 / 42.0 = 1.0
+    test(`OP_DIV, 32'h7F7F_FFFF, 32'h7F7F_FFFF, 32'h3F80_0000);  // 3.4e38 / 3.4e38 = 1.0
+    test(`OP_DIV, 32'h425A_CCCD, 32'h0E69_999A, 32'h736F_C7E2);  // 54.7 / 2.87e-30 ~= 1.89e31
+    test(`OP_DIV, 32'h0000_0040, 32'h0000_0003, 32'h41B4_0000);  // 9e-44 / 4e-45 = 22.5
+    test(`OP_DIV, 32'h4049_0FDB, 32'h402D_F854, 32'h3F93_EEE0);  // pi / e = ~1.557
 
     // Special cases
-    test(`OP_DIV, `ZERO, `ZERO, `ZERO);  // 0.0 * 0.0 = 0.0
-    test(`OP_DIV, `ZERO, `NEG_ZERO, `NEG_ZERO);  // 0.0 * (-0.0) = -0.0
-    test(`OP_DIV, `INF, 32'h4010_0000, `INF);  // Inf * 2.25 = Inf
-    test(`OP_DIV, 32'h4010_0000, `INF, `INF);  // 2.25 * Inf = Inf
-    test(`OP_DIV, `INF, `NEG_INF, `NEG_INF);  // Inf * (-Inf) = -Inf
-    test(`OP_DIV, `NEG_INF, 32'h4010_0000, `NEG_INF);  // -Inf * 2.25 = -Inf
-    test(`OP_DIV, `NAN, 32'hC188_28F6, `NAN);  // NaN * 42.3 = NaN, invalid
-    test(`OP_DIV, `NAN, `NAN, `NAN);  // NaN * NaN = NaN, invalid
+    test(`OP_DIV, 32'h3F80_0000, `INF, `ZERO);  // 1.0 / Inf = 0.0
+    test(`OP_DIV, 32'hC048_F5C3, `INF, `NEG_ZERO);  // -3.14 / Inf = -0.0
+    test(`OP_DIV, `ZERO, `ZERO, `NAN);  // 0.0 / 0.0 = NaN
+    test(`OP_DIV, `ZERO, `NEG_ZERO, `NAN);  // 0.0 / (-0.0) = NaN
+    test(`OP_DIV, `INF, 32'h4010_0000, `INF);  // Inf / 2.25 = Inf
+    test(`OP_DIV, `NEG_INF, 32'h4010_0000, `NEG_INF);  // -Inf / 2.25 = -Inf
+    test(`OP_DIV, `NAN, 32'hC188_28F6, `NAN);  // NaN / 42.3 = NaN, invalid
+    test(`OP_DIV, `NAN, `NAN, `NAN);  // NaN / NaN = NaN, invalid
 
     $display("");
     #30 $finish();
